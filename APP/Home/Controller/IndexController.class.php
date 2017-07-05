a55fc358f39b9960066d7b0084a2cdc16a1558db
wujiaqiang <2227920646@qq.com>
2017-07-05 09:54:39 +0800

    qqq

--- APP/Home/Controller/IndexController.class.php
+++ APP/Home/Controller/IndexController.class.php
@@ -97,7 +97,7 @@ class IndexController extends Controller
         }
         $res=$model->select();
         $this->assign('res', $res);
-        $this->display();
+        $this->display();//zhushi
     }
     
     public function add_notice(){
